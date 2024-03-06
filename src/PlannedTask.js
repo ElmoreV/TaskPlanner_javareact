0ad7a8d795127cba9518d6051dc549926109452e
Elmore Vaal <vaal@ph2.uni-koeln.de>
2024-03-06 20:54:09 +0100

    Adding topic helper and the task for in the weekly/planned view

---
+++ src/PlannedTask.js
@@ -0,0 +1,107 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { find_topic_by_key_r } from './TopicHelper';
+
+const PlannedTask = (props) => {
+    const { taskKey, taskName, setTaskName, deleteTask, completed, completeTask, currentTopic, changeTopic, planned, plan, topics, taskTopics } = props;
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [color, setColor] = useState('green');
+    const [isDragging, setIsDragging] = useState(false);
+    const [isDraggingAllowed, setIsDraggingAllowed] = useState(true);
+
+
+
+    const handleChange = (e) => {
+        console.info(e.target.value);
+        setTaskName(e.target.value);
+    }
+
+    const toggleEdit = () => {
+        setIsEditing(true);
+        setIsDraggingAllowed(false);
+        // TODO: set focus on text edit box
+
+    }
+
+    const handleDragStart = (e) => {
+        setIsDragging(true)
+        e.dataTransfer.setData('Text', taskKey)
+        e.dataTransfer.setData('Text2', currentTopic)
+        setColor('blue')
+    }
+    const handleDragEnd = () => {
+        setIsDragging(false)
+        setColor('green')
+    }
+
+    const handleDrop = (e) => {
+        e.preventDefault()
+        e.target.setAttribute('draggedOver', false)
+        console.info('drop')
+        setColor('yellow')
+        var key = e.dataTransfer.getData("Text")
+        var oldTopic = e.dataTransfer.getData("Text2")
+        if (changeTopic) {
+            changeTopic(key, oldTopic, currentTopic)
+        }
+    }
+    const handleDragOver = (e) => {
+        e.preventDefault();
+        // this is not perfect, because I always want the <div class='task'> to be the target..
+        e.target.setAttribute('draggedOver', true);
+        setColor('red');
+    }
+    const handleDragLeave = (e) => {
+        e.preventDefault();
+        e.target.setAttribute('draggedOver', false);
+        setColor('gray');
+    }
+
+    const moveToWeek = () => {
+        plan()
+    }
+
+    const handleBlur = () => {
+        setIsDraggingAllowed(true);
+        setIsEditing(false);
+
+    }
+    let class_str = 'task'
+    // Completion has precedence over planned
+    if (completed) { class_str = 'taskCompleted' }
+    else if (planned) { class_str = 'taskPlanned' }
+
+    const dragHandlers = isDraggingAllowed ? { draggable: true, onDragStart: handleDragStart, onDragEnd: handleDragEnd } : {};
+    const dropHandlers = isDragging ? {} : { onDrop: handleDrop, onDragOver: handleDragOver, onDragLeave: handleDragLeave };
+
+
+    return (<div className={class_str}
+        onDoubleClick={toggleEdit}
+        {...dragHandlers}
+        {...dropHandlers}>
+        {isEditing ?
+            <input type='text'
+                value={taskName}
+                onChange={handleChange}
+                onBlur={handleBlur} /> :
+            //  <span style={{color : color}}>{taskName}</span>
+            <span style={{ color: color }}>{taskName}</span>
+        }
+        {deleteTask && (<button className='taskDelete' onClick={deleteTask && deleteTask}>Delete</button>)}
+        {!completed && completeTask && (<button className='taskComplete' onClick={completeTask}>Complete</button>)}
+        {completed && completeTask && (<button className='taskComplete' onClick={completeTask}>Decomplete</button>)}
+        {plan && (<button className='moveToWeek' onClick={moveToWeek}> Plan for this week </button>)}
+    </div>);
+}
+
+PlannedTask.propTypes = {
+    taskName: PropTypes.string.isRequired,
+    setTaskName: PropTypes.func,
+    deleteTask: PropTypes.func,
+    completed: PropTypes.bool.isRequired,
+    completeTask: PropTypes.func
+};
+
+
+export default PlannedTask;
---
+++ src/TopicHelper.js
@@ -0,0 +1,120 @@
+
+
+const find_topic_by_key_r = (topics, topic_key) => {
+    console.debug(topics);
+    for (let topic of topics) {
+        console.debug(topic.id);
+        if (topic.id === topic_key) {
+            console.debug('Fount it!');
+            return topic;
+        }
+        let topic_res = find_topic_by_key_r(topic.subtopics, topic_key);
+        if (topic_res) { console.debug('Bubble up'); return topic_res; }
+    }
+    return null;
+}
+const find_topic_by_name_r = (topics, topic_name) => {
+    console.debug(topics);
+    for (let topic of topics) {
+        console.debug(topic.name);
+        if (topic.title === topic_name) {
+            console.debug('Fount it!');
+            return topic;
+        }
+        let topic_res = find_topic_by_name_r(topic.subtopics, topic_name);
+        if (topic_res) { console.debug('Bubble up'); return topic_res; }
+    }
+    return null;
+}
+
+const getFreeTaskKey = (tasks) => {
+    return 1 + tasks.reduce((max_key, task) => Math.max(max_key, task.key), 0);
+}
+
+function isEqual(a, b) {
+    if (a.length !== b.length) {
+        return false;
+    }
+    let map = new Map();
+    for (let elem of a) {
+        map.set(elem, (map.get(elem) || 0) + 1)
+
+    }
+    for (let elem of b) {
+        if (!map.has(elem)) {
+            return false;
+        }
+        map.set(elem, map.get(elem) - 1);
+        if (map.get(elem) < 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+const topicCompletelyContainsTasks = (topic, task) => {
+    // Check all topics and if the task is only contained in topic
+    // and/or its subtopics, we can return true
+    // We check this by checking the topics in the task, and seeing if all the topics are contained within
+    // the topic and its subtopics (recursively)
+    let included_topics = []
+    if (task.topics.includes(topic.title)) {
+        included_topics.append(topic.title)
+        if (isEqual(task.topics, included_topics)) { return true; }
+    }
+    // try all subtopics
+
+    included_topics.append(topic.subtopics.map((topic) => topicCompletelyContainsTasks(topic, task)))
+    console.log(included_topics)
+    if (isEqual(task.topics, included_topics)) { return true; }
+    return false;
+}
+
+const filter_by_name_r = (topics, topic_name) => {
+    // 1. enumerate all subtopics that do not match name, and filter their subtopics
+    // 2. filter all subtopics that do match name
+    // 3. return the topics object as is, just with all topics with title==topic_name filtered out,
+    // and all subtopics (or subsubtopics) with title==topic_name filtered out
+    console.log(topics)
+    return topics.filter((topic) => topic.title !== topic_name).map(
+        (topic) => { return { ...topic, subtopics: filter_by_name_r(topic.subtopics, topic_name) } }
+    )
+
+
+    // return topic.subtopics
+}
+
+const get_all_subtopics = (topic) => {
+
+    return topic.subtopics.map((subtopic) => get_all_subtopics(subtopic)).concat(topic);
+}
+const find_topic_by_name = (topics, topic_name) => {
+    return find_topic_by_name_r(topics, topic_name);
+}
+
+const isTaskInAnyTopic = (task, topics) => {
+    // check if the topic of the task in the
+    console.log(task.topics)
+    task.topics = task.topics.filter((topic_name) => {
+        console.log('Is topic .. in non-deleted topics ...')
+        console.log(topic_name)
+        console.log(topics)
+        return find_topic_by_name(topics, topic_name)
+    })
+    console.log('Resulting task.topics')
+    console.log(task.topics)
+    console.log(task.topics.length)
+    if (task.topics.length > 0) {
+        return true;
+    }
+    return false;
+}
+
+
+export default find_topic_by_key_r;
+export { find_topic_by_key_r };
+export { find_topic_by_name_r };
+export { getFreeTaskKey };
+export { isTaskInAnyTopic };
+export { filter_by_name_r };
