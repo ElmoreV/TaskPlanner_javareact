75d9af6a442a4d50ecd55633748266834e3d944a
Elmore Vaal <vaal@ph2.uni-koeln.de>
2024-03-11 19:44:47 +0100

    Added a way to hide completed items in the weekly/planned list

--- src/PlannedList.js
+++ src/PlannedList.js
@@ -6,23 +6,41 @@ import PlannedTask from './PlannedTask';
 const PlannedList = (props) => {
     const { tasks, setTasks, topics, setTopics } = props;
 
+    const [hideCompletedItems, setHideCompletedItems] = useState(false)
+    const onHideCompletedItemsChange = () => {
+        setHideCompletedItems(!hideCompletedItems)
+    }
+
+
     return (
         <div className='planned-list'>
+            <label><input
+                type="checkbox"
+                name='HideCompletedItems'
+                onChange={onHideCompletedItemsChange}
+                className="form-check-input"
+            />Hide completed tasks</label>
             <ul key='root_topics'>
                 {tasks.map(
                     (task) => {
-                        return (<li>{task.thisWeek && <PlannedTask
-                            taskName={task.name}
-                            taskKey={task.id}
-                            // setTaskName={getSetTaskNameFunc(task.id)}
-                            // deleteTask = {getDeleteTask(task.id)}
-                            completed={task.completed}
-                            // completeTask = {getCompleteTask(task.id)}
-                            // currentTopic = {task.topics[0]}
-                            topics={topics}
-                            taskTopics={task.topics}
-                        // changeTopic = {getChangeTaskTopic()}
-                        />} </li>)
+                        return (
+                            <li>
+                                {!(task.completed && hideCompletedItems) &&
+                                    task.thisWeek && <PlannedTask
+                                        taskName={task.name}
+                                        taskKey={task.id}
+                                        // setTaskName={getSetTaskNameFunc(task.id)}
+                                        // deleteTask = {getDeleteTask(task.id)}
+                                        completed={task.completed}
+                                        // completeTask = {getCompleteTask(task.id)}
+                                        // currentTopic = {task.topics[0]}
+                                        topics={topics}
+                                        taskTopics={task.topics}
+                                    // changeTopic = {getChangeTaskTopic()}
+                                    />
+                                }
+                            </li>
+                        )
                     }
                 )}
             </ul>
