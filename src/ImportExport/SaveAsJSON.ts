c697715ae5ac341f7dfe3090f02145d53d516267
Elmore Vaal <vaal@ph2.uni-koeln.de>
2025-10-17 15:37:30 +0200

    updated save function with new data conversion

--- src/ImportExport/SaveAsJSON.ts
+++ src/ImportExport/SaveAsJSON.ts
@@ -1,33 +1,16 @@
-import {
-  convert_v1_to_v2,
-  convert_v2_to_v1,
-} from "../Converters/Migration_V1_V2/UpdateV1ToV2.ts";
 import { AppData, AppDataV1, AppDataV2 } from "../Structure/AppDataTypes.ts";
 import { saveFile } from "./LoadFile.ts";
 import { Version } from "../Structure/Versions.ts";
-import { getVersionOfAppData, versionToString } from "./VersionDeterminer.ts";
+import { versionToString } from "./VersionDeterminer.ts";
+import { convertAppdata } from "../Converters/VersionConversion.ts";
 
 export const exportJSON = (
   appData: AppData,
   fileNameRef,
   asVersion: Version
 ) => {
-  const curVersion = getVersionOfAppData(appData);
-  let appDataConverted: AppData = appData;
-  if (curVersion !== asVersion) {
-    if (curVersion === Version.V1 && asVersion === Version.V2) {
-      const appDataV1: AppDataV1 = appData as AppDataV1;
-      appDataConverted = convert_v1_to_v2(appDataV1.tasks, appDataV1.topics);
-    } else if (curVersion === Version.V2 && asVersion === Version.V1) {
-      const appDataV2: AppDataV2 = appData as AppDataV2;
-      appDataConverted = convert_v2_to_v1(
-        appDataV2.taskMap,
-        appDataV2.tagMap,
-        appDataV2.tagTasksMap,
-        appDataV2.plannedTaskIdList
-      );
-    }
-  }
+  let appDataConverted = convertAppdata(appData, asVersion);
+
   if (asVersion === Version.V1) {
     const appDataV1: AppDataV1 = appDataConverted as AppDataV1;
     const jsonContent = formatJSONfromV1(appDataV1.topics, appDataV1.tasks);
